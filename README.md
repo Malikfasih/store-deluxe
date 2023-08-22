bbc4abf99d860e6a2a24d6d9ca827e63445385ab
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-08-22 17:12:53 +0500

    final touch

---
+++ README.md
@@ -0,0 +1,11 @@
+# Full Stack E-Commerce Deluxe store: Next.js 13 App Router, React, Tailwind
+
+![deluxe-store](https://github.com/Malikfasih/store-deluxe/assets/90088021/086de3eb-8d1c-4c22-b4fb-b197506159c3)
+
+## Key Features:
+
+- Designed Deluxe-store app with headless-ui and lucide-react!
+- Used Zustand for fast and scalable state-management.
+- You will be able to see filtered product items with such as "Color" and "Size"
+- Used react-hot-toast for smoking hot React notifications.
+- Payment handle through Stripe checkout
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,26 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  // const billboard = await getBillboard("3cbc6295-206d-44ef-ad33-5e438d359d98");
+  const billboard = await getBillboard("b40abb8f-d10f-4eca-afb9-83c62190ac54");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
